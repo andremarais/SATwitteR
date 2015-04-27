7fc53ea66e182eac86f8ef4a6e148977a86c0c8e
Andre Marais <agmarais@gmail.com>
2015-04-27 22:29:45 +0200

    soveel kak om uit te haal

--- script.R
+++ script.R
@@ -1,6 +1,12 @@
 require(twitteR)
-
+require(plyr)
+require(wordcloud)
+require(tm)
+#PC
 setwd("C:/Users/Veldrin/Documents/GitHub/SATwitteR")
+
+
+#Laptop
 setwd("C:/Users/anmarais/Desktop/GitHub/SATwitteR")
 
 
@@ -22,14 +28,16 @@ all.tweets <- list()
 user.tweets <- data.frame()
 combined.tweets <- list()
 
-for (i in 1:nrow(t.handles)) {
+# for (i in 1:nrow(t.handles)) {
+for (i in 1:15) {
   #user.tweets <- userTimeline(as.character(t.handles[i,1]), n = 200)
   all.tweets[[i]] <- try(userTimeline(as.character(t.handles[i,1]), n = 200))
   
 }
 
 k <- 1
-for (i in 1:nrow(t.handles)) {
+# for (i in 1:nrow(t.handles)) {
+for (i in 1:15) {
 if (length(all.tweets[[i]]) > 1) {
   user.tweets <- data.frame()
   for (j in 1:length(all.tweets[[i]])) {
@@ -43,3 +51,27 @@ if (length(all.tweets[[i]]) > 1) {
   k <- k +1
 } 
 }
+
+df <- ldply(combined.tweets, data.frame)
+
+colnames(df) <- c("ScreenName", "Tweet", "TweetDate")
+
+#remove all direct tweets 
+df <- df[-which(as.character(gregexpr("@", df$Tweet)) != -1),]
+
+tweet.corpus <- Corpus(VectorSource(df$Tweet))
+
+april.tweets <- df[which(df$TweetDate >= "2015-04-01"),]
+april.tweets <- april.tweets[40:50,]
+tweet.corpus <- Corpus(VectorSource(april.tweets$Tweet))
+tdm <- TermDocumentMatrix(tweet.corpus)
+
+
+
+tweet.corpus <- tm_map(tweet.corpus, removeWords, stopwords("en"))
+tweet.corpus <- tm_map(tweet.corpus, removeWords, c("&lsquo;", "&rsquo;", "&ldquo;", "&ldquo;", "&ndash", "said"))
+tweet.corpus <- tm_map(tweet.corpus, removePunctuation)
+tweet.corpus <- tm_map(tweet.corpus, stripWhitespace)
+tweet.corpus <- tm_map(tweet.corpus, removePunctuation)
+tweet.corpus <- tm_map(tweet.corpus, removeNumbers)
+
