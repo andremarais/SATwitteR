4945c0a47962ffc2432b0b89c308b34306c83c22
Andre Marais <agmarais@gmail.com>
2015-04-30 10:30:47 +0200

    ants!!!

--- script.R
+++ script.R
@@ -28,16 +28,14 @@ all.tweets <- list()
 user.tweets <- data.frame()
 combined.tweets <- list()
 
-# for (i in 1:nrow(t.handles)) {
-for (i in 1:15) {
+ for (i in 1:nrow(t.handles)) {
   #user.tweets <- userTimeline(as.character(t.handles[i,1]), n = 200)
-  all.tweets[[i]] <- try(userTimeline(as.character(t.handles[i,1]), n = 200))
-  
+  all.tweets[[i]] <- try(userTimeline(as.character(t.handles[i,1]), n = 500))
+  print(i)
 }
 
 k <- 1
-# for (i in 1:nrow(t.handles)) {
-for (i in 1:15) {
+ for (i in 1:nrow(t.handles)) {
 if (length(all.tweets[[i]]) > 1) {
   user.tweets <- data.frame()
   for (j in 1:length(all.tweets[[i]])) {
@@ -57,40 +55,39 @@ df <- ldply(combined.tweets, data.frame)
 colnames(df) <- c("ScreenName", "Tweet", "TweetDate")
 
 #remove all direct tweets 
-df <- df[-which(as.character(gregexpr("@", df$Tweet)) != -1),]
+df <- df[which(as.character(gregexpr("@", df$Tweet)) != -1),]
 
-df <- df[which(df$TweetDate >= "2015-04-01"),]
+#df <- df[which(df$TweetDate >= "2015-04-01"),]
 
 # removes emoticon bullshit
-df$Tweet <- sapply(april.tweets$Tweet, function(x) iconv(x,"latin1", "ASCII", sub = ""))
-df$Tweet <- sapply(april.tweets$Tweet, function(x) gsub("\n", "", x))
-
-
-
-
-
+df$Tweet <- sapply(df$Tweet, function(x) iconv(x,"latin1", "ASCII", sub = ""))
+df$Tweet <- sapply(df$Tweet, function(x) gsub("\n", "", x))
+df$Tweet <- sapply(df$Tweet, function(x) gsub("the", "", x, ignore.case = T)) #removeWords is buggy now, doesnt remove "the" when it's the first word of the string
 
 tweet.corpus <- Corpus(VectorSource(df$Tweet))
 
-tweet.corpus <- tm_map(tweet.corpus, removeWords, stopwords("en"))
-tweet.corpus <- tm_map(tweet.corpus, removeWords, c("&lsquo;", "&rsquo;", "&ldquo;", "&ldquo;", "&ndash", "said", "the"))
-tweet.corpus <- tm_map(tweet.corpus, removePunctuation)
+tweet.corpus <- tm_map(tweet.corpus, tolower)
+tweet.corpus <- tm_map(tweet.corpus, removeWords, stopwords("english"))
+tweet.corpus <- tm_map(tweet.corpus, removeWords, c("&lsquo;", "&rsquo;", "&ldquo;", "&ldquo;", "&ndash", "said"))
+#tweet.corpus <- tm_map(tweet.corpus, removePunctuation)
 tweet.corpus <- tm_map(tweet.corpus, stripWhitespace)
-tweet.corpus <- tm_map(tweet.corpus, removePunctuation)
-tweet.corpus <- tm_map(tweet.corpus, removeNumbers)
-tweet.corpus <- tm_map(tweet.corpus, removeWords, stopwords("en"))
-wordcloud(tweet.corpus, min.freq = 4)
+#tweet.corpus <- tm_map(tweet.corpus, removeNumbers)
 
 
 
 
-tdm <- TermDocumentMatrix(tweet.corpus,control=list(weighting=weightTfIdf)) 
-#'
+
+tdm <- TermDocumentMatrix(tweet.corpus) 
 m <- as.matrix(tdm)
-#'
 v <- sort(rowSums(m),decreasing=TRUE)
-#'
+head()
+wordcloud(names(v),v,c(4,.2),2,100)
 
-#'
-wordcloud(names(v),v,c(4,.2),2,200)
 
+
+
+b <- regmatches(df$Tweet,gregexpr("#(\\d|\\w)+", df$Tweet))
+c <- b[which(as.character(b) != "character(0)")]
+d <- table(unlist(tolower(c)))
+e <- order(d, decreasing = T)[1:6]
+d[e]
